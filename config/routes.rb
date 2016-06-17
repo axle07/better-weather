a1c81150216c67ea907656dee7fed25c668cf89a
Alex <meetalexjohnson@gmail.com>
2016-06-16 20:46:34 -0700

    Adding Google Search Console

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 	
-	root 'home#index'
+  root 'home#index'
   resources :locations
-	get '/search/:weather' => 'locations#search', as: "search"
+  get '/search/:weather' => 'locations#search', as: "search"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
